b7d1491a64e08c1daf285541987c1b9135950fa5
Duong Hoang <dtht2d@outlook.com>
2023-02-22 14:39:53 -0600

    wigner_D function update

--- codes/methods/Bi_SO4.py
+++ codes/methods/Bi_SO4.py
@@ -1,27 +1,59 @@
+import numpy as np
+import cmath
 
-class Wigner_D (j,m,mp,theta_0,theta,phi):
-    '''
+def fact(n):
+    """
+    This function is used to calculate factorial of a number by using an iterative approach instead of recursive approach
+    """
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+
+class Wigner_D:
+    """
     Args:
         j (scalar): angular momentum
         m (scalar): eigenvalue of angular momentum
         mp (scalar): eigenvalue of j along rotated axis
-        theta_0 (scalar): fist angle of rotation [0,pi]
-        theta (scalar): second angle of rotation [0,pi]
-        phi (scalar): third angle of rotation [0,2pi]
-    Returns: complex number, wigner D function
+        theta_0 (scalar): first angle of rotation [0, pi]
+        theta (scalar): second angle of rotation [0, pi]
+        phi (scalar): third angle of rotation [0, 2*pi]
+    Returns: complex number, Wigner D function
     ==========================Reference==================================
-    [5] Chapter 4-(p.76,eq.1&3)  D.A. Varshalovich, A.N. Moskalev, V.K Khersonskii,
+    [5] Chapter 4.3-(p.76,eq.1)  D.A. Varshalovich, A.N. Moskalev, V.K Khersonskii,
         Quantum Theory of Angular Momentum (1988)
-    '''
-    def __init__(self,j,m,mp,theta_0,theta,phi):
+    """
+
+    def __init__(self, j, m, mp, theta_0, theta, phi):
         self.j = j
         self.m = m
         self.mp = mp
         self.theta_0 = theta_0
         self.theta = theta
         self.phi = phi
-    def fact(seln):
-        if n == 0:
-            return 1
-        else:
-            return n * self.factorial(n-1)
+
+    def compute_dsmall(self):
+        """
+        This method is used to calculate the Wigner d small- real function involving trigonometric functions
+        ==========================Reference==================================
+        [5] Chapter 4.3.1-(p.76,eq.3)  D.A. Varshalovich, A.N. Moskalev, V.K Khersonskii,
+        Returns: Wigner d - real function
+        """
+        kmax = max(0, self.m - self.mp)
+        kmin = min(self.j + self.m, self.j - self.mp)
+        term1 = np.sqrt(fact(self.j + self.m) * fact(self.j - self.m) * fact(self.j + self.mp) * fact(self.j - self.mp))
+        s = 0
+        for k in range(kmax, kmin + 1):
+            numerator = (-1) ** k * (cmath.cos(self.theta / 2)) ** (2 * self.j - 2 * k + self.m - self.mp) * \
+                        (cmath.sin(self.theta / 2)) ** (2 * k - self.m + self.mp)
+            denominator = fact(k) * fact(self.j + self.m - k) * fact(self.j - self.mp - k) * fact(self.mp - self.m + k)
+            s += numerator / denominator
+        return s
+
+    def wigner_D(self):
+        term1 = np.exp(-1j * self.m * self.theta_0)
+        term2 = self.compute_dsmall()
+        term3 = np.exp(-1j * self.mp * self.phi)
+        return term1 * term2 * term3
--- codes/tests/test_WignerD_calc.py
+++ codes/tests/test_WignerD_calc.py
@@ -1,4 +1,9 @@
 """
 Compare the Wigner D matrix calculation with the one from Sympy, PyXtal_FF and my own
 """
-from sympy import *
+import numpy as np
+from methods.Bi_SO4 import Wigner_D as wd
+j, m, mp, theta_0, theta,phi= 1, 1, 0, np.pi, np.pi/2, 0
+
+wD = wd.wigner_D((j, m, mp, theta_0, theta, phi))
+print(wD)
