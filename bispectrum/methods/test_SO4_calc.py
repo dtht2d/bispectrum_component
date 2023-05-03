52769a501e737151acb47a2d7ba093201d17508e
Duong Hoang <dtht2d@outlook.com>
2023-05-02 19:37:28 -0500

    SO4 Bispectrum class function update

--- bispectrum/methods/test_SO4_calc.py
+++ bispectrum/methods/test_SO4_calc.py
@@ -1,5 +1,5 @@
 from bispectrum.methods.calc.INPUT_param import  neighbor_params
-from bispectrum.methods.calc.SO4 import B
+from bispectrum.methods.calc.SO4 import Bispectrum
 from sympy import *
 import timeit
 from sympy.physics.quantum.cg import CG
@@ -13,7 +13,7 @@ output_directory = '/Users/duonghoang/Documents/GitHub/bispectrum_component/data
 data = neighbor_params(center_atom_id, r_mu, R_cut, input_file_path, output_directory, file_type='cif')
 
 #possible list of m values
-B = B(j=1,j1=2,j2=3, params=data)
+B = Bispectrum(j=1,j1=2,j2=3, params=data)
 list_generate = B.generate_m_values(3,1,2)
 print (list_generate)
 j1, j2, j, m1, m2, m = 1/2, 1/2, 1, -1/2, -1/2, -1
@@ -21,7 +21,5 @@ CG_coeff = B.cg(j1, j2, j, m1, m2, m)
 print(CG_coeff)
 
 #Compare sympy
-t0=timeit.default_timer()
 cg = CG(j1,m1,j2,m2,j,m)
 cg = cg.doit()
-time for CG function from Sympy:", t1-t0, "seconds")
