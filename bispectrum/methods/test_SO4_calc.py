c368d5b6c536cfa7a2dc0edb938e39f1d97b2a06
Duong Hoang <dtht2d@outlook.com>
2023-05-04 14:40:33 -0500

    SO4 Bispectrum class function update

--- bispectrum/methods/test_SO4_calc.py
+++ bispectrum/methods/test_SO4_calc.py
@@ -41,5 +41,12 @@ print(U)
 u = B.u_small(j, m, mp, params=data)
 print(u)
 
+
+#Test bispectrum
+B = Bispectrum(j=2, j1=3/2, j2=1/2, params=data)
 Bispectrum_SO4 = B.evaluate(j=2, j1=3/2, j2=1/2, params=data)
+print(Bispectrum_SO4)
+
+B = Bispectrum(j=2, j1=1/2, j2=3/2, params=data)
+Bispectrum_SO4 = B.evaluate(j=2, j1=1/2, j2=3/2, params=data)
 print(Bispectrum_SO4)
