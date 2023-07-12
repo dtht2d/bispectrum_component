fb608e61b1f17142f7b51889e057c80c106a1cbb
Duong Hoang <dtht2d@outlook.com>
2023-07-12 01:16:04 -0500

    test bispec

--- bispectrum/methods/tests/test_SO4_calc.py
+++ bispectrum/methods/tests/test_SO4_calc.py
@@ -16,43 +16,41 @@ data = neighbor_params(center_atom_id, r_mu, R_cut, input_file_path, output_dire
 #possible list of m values
 B = Bispectrum(j=2, j1=3/2, j2=1/2, params=data)
 keep_list, full_list = B.generate_m_values(3,1,2)
-print (keep_list)
-j1 = 1
-m1 = 1
-j2 = 1/2
-m2 = 1/2
-j = 3/2
-m = 3/2
-m1p =-1
-m2p = 1/2
-mp = 1/2
+print ("test generate set", keep_list)
+
 
 #test Clebsch Gordan Coefficient calculation
+j1, j2, j, m1, m2, m = 2,1/2,5/2,1,1/2,3/2
 CG_coeff = B.clebsch_gordan(j1, j2, j, m1, m2, m)
-print(CG_coeff)
+print("test CG calculationg from our function", CG_coeff)
 
-CG_coeff_p = B.clebsch_gordan(j1, j2, j, m1p, m2p, mp)
-print(CG_coeff_p)
 #Compare sympy
 cg = CG(j1,m1,j2,m2,j,m)
 cg = cg.doit()
+print("CG calculation from Sympy",cg)
 
 #test Coupling coefficient calculation
+m1p, m2p, mp = 1, 1/2, 3/2
 H_coeff = B.H(j1, j2, j, m1, m2, m, m1p, m2p, mp)
-print(H_coeff)
+print("test coupling coefficient calculation",H_coeff)
 
 #Test Wigner D
-j, m, mp, theta_0, theta,phi= 1, -1, 0, np.pi, np.pi/2, 0
-WD = B.wigner_D(j, m, mp, theta_0, theta,phi)
+j1, m1, m1p, theta_0, theta,phi= 3/2, 3/2, -3/2, np.pi/4, np.pi/3, np.pi/4
+WD = B.wigner_D(j1, m1, m1p, theta_0, theta,phi)
+print ("test Wigner_D calculation",WD)
+
 #Test U_rot
-U = B.U_rot(j, m, mp, theta_0, theta,phi)
-print (WD)
-print(U)
+U = B.U_rot(j1, m1, m1p, theta_0, theta,phi)
+print("test U_rot calculation", U)
+
 #Test u
 u = B.u_small(j, m, mp, params=data)
-print(u)
+print("test u calculation",u)
+
 
 #Test bispectrum
 B = Bispectrum(j=2, j1=3/2, j2=1/2, params=data)
 Bispectrum_SO4 = B.evaluate(j=2, j1=3/2, j2=1/2, params=data)
-print(Bispectrum_SO4)
+print("Bispectrum coeeficient", Bispectrum_SO4)
+
+
