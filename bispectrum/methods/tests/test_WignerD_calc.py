38db82895bc8bcb7d78fb5b0f20c4687cede4632
Duong Hoang <dtht2d@outlook.com>
2023-07-12 01:12:56 -0500

    test Wigner_D

--- bispectrum/methods/tests/test_WignerD_calc.py
+++ bispectrum/methods/tests/test_WignerD_calc.py
@@ -17,7 +17,7 @@ print ("Wigner_D calculation from our function", wd)
 t1=timeit.default_timer()
 print("Execution time for Wigner_D function from calc.WignerD:", t1 - t0, "seconds")
 
-#D(phi,theta, -phi) D(theta,-theta, -phi)
+#test D(phi,theta, -phi) D(theta,-theta, -phi)
 WD_1 = Wigner_D(j, m, mp, phi, theta,-phi)
 wd_1 = WD_1.wigner_D()
 print ("Wigner_D term 1", wd_1)
@@ -25,7 +25,8 @@ print ("Wigner_D term 1", wd_1)
 WD_1p = Wigner_D(j, m, mp, phi, -theta,-phi)
 wd_1p = WD_1p.wigner_D()
 print ("Wigner_D term 3", wd_1p)
-#d_small
+
+#test d_small
 WD_1= Wigner_D(j, m, mp, theta_0, theta,phi)
 d_small = WD_1.compute_dsmall()
 print ("d_small from calc", d_small)
@@ -35,10 +36,11 @@ print ("d_small from table", d_small)
 #Case1 j=3/2, m=3/2, mp=-3/2
 j, m, mp, theta_0, theta,phi= 3/2, 3/2, -3/2, np.pi/3, np.pi/2, np.pi/4
 
-#rotaion matrix U_jmmp
+#Test rotaion matrix U_rot
 U_calc_1 = U_rot(j, m, mp, theta_0, theta,phi)
 U_table_1= 1j*((np.sin(theta_0/2)*np.sin(theta)*(np.exp(-1j*phi)))**3)
 print("U_table_1", U_table_1)
 print("U_calc_1", U_calc_1)
+
 
 
