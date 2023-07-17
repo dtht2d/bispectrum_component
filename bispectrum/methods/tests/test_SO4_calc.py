9b403d083bc2548fd2a780aaff55689a4547a177
Duong Hoang <dtht2d@outlook.com>
2023-07-16 23:07:08 -0500

    condition update

--- bispectrum/methods/tests/test_SO4_calc.py
+++ bispectrum/methods/tests/test_SO4_calc.py
@@ -1,5 +1,6 @@
 from bispectrum.methods.calc.INPUT_param import  neighbor_params
-from bispectrum.methods.calc.SO4 import Bispectrum
+from bispectrum.methods.calc.SO4 import *
+import pandas as pd
 from sympy import *
 import timeit
 from sympy.physics.quantum.cg import CG
@@ -12,45 +13,31 @@ input_file_path = path
 output_directory = '/Users/duonghoang/Documents/GitHub/bispectrum_component/data'
 #read data from file
 data = neighbor_params(center_atom_id, r_mu, R_cut, input_file_path, output_directory, file_type='cif')
-
-#possible list of m values
-B = Bispectrum(j=2, j1=3/2, j2=1/2, params=data)
-keep_list, full_list = B.generate_m_values(3,1,2)
-print ("test generate set", keep_list)
-
-
-#test Clebsch Gordan Coefficient calculation
-j1, j2, j, m1, m2, m = 2,1/2,5/2,1,1/2,3/2
-CG_coeff = B.clebsch_gordan(j1, j2, j, m1, m2, m)
-print("test CG calculationg from our function", CG_coeff)
-
-#Compare sympy
-cg = CG(j1,m1,j2,m2,j,m)
-cg = cg.doit()
-print("CG calculation from Sympy",cg)
-
-#test Coupling coefficient calculation
-m1p, m2p, mp = 1, 1/2, 3/2
-H_coeff = B.H(j1, j2, j, m1, m2, m, m1p, m2p, mp)
-print("test coupling coefficient calculation",H_coeff)
-
-#Test Wigner D
-j1, m1, m1p, theta_0, theta,phi= 3/2, 3/2, -3/2, np.pi/4, np.pi/3, np.pi/4
-WD = B.wigner_D(j1, m1, m1p, theta_0, theta,phi)
-print ("test Wigner_D calculation",WD)
-
-#Test U_rot
-U = B.U_rot(j1, m1, m1p, theta_0, theta,phi)
-print("test U_rot calculation", U)
-
-#Test u
-u = B.u_small(j, m, mp, params=data)
-print("test u calculation",u)
-
-
+df= pd.DataFrame(data)
+print(df)
 #Test bispectrum
-B = Bispectrum(j=2, j1=3/2, j2=1/2, params=data)
-Bispectrum_SO4 = B.evaluate(j=2, j1=3/2, j2=1/2, params=data)
-print("Bispectrum coeeficient", Bispectrum_SO4)
+B = Bispectrum(j=5/2, j1=2, j2=1/2, params=data)
+Bispectrum_SO4 = B.evaluate(j=5/2, j1=2, j2=1/2, params=data)
+print("Bispectrum coefficient for atom ID17", Bispectrum_SO4)
+
+
+#test generate set+Clebsch-Gordan coefficients
+keep_set, full_set = B.generate_m_values(5/2,2,1/2)
+keep_set_arr = np.array(keep_set)
+print ("test generate set", keep_set_arr.shape)
+df = pd.DataFrame(keep_set, columns=["m1", "m2", "m", "m1p", "m2p", "mp"])
+unique_combinations = df[["m1", "m2", "m"]].drop_duplicates()
+CG_unique =[]
+j=5/2
+j1=2
+j2=1/2
+for _, row in unique_combinations.iterrows():
+    m, m1, m2 = row["m"], row["m1"], row["m2"]
+    CB = B.clebsch_gordan(j1,j2,j,m1,m2,m)
+    CG_unique.append(CB)
+unique_combinations['CG'] = CG_unique
+print(unique_combinations)
+
+
 
 
